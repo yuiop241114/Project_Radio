2a9e1f12d7f521da38c005923c0300d8c420e63b
kjsHome <was1239@gmail.com>
2025-11-23 20:44:23 +0900

    bat 파일을 생성해서 React 폴더 생성

---
+++ frontend/create-folder.bat
@@ -0,0 +1,24 @@
+@echo off
+echo 📁 Creating React project folder structure...
+
+mkdir src\api
+mkdir src\components
+mkdir src\components\common
+mkdir src\components\chat
+mkdir src\components\radio
+mkdir src\components\posts
+
+mkdir src\hooks
+
+mkdir src\pages
+
+mkdir src\router
+
+mkdir src\store
+
+mkdir src\styles
+
+mkdir src\utils
+
+echo Folder structure created successfully! 🎉
+pause
