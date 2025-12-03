46018b5660f821d9da0c5f7271bff468dfcab78a
kjsHome <was1239@gmail.com>
2025-12-03 22:19:34 +0900

    로그인 기능 컨트롤, DTO 작성 완료

---
+++ backend/cast/cast/src/main/java/com/radio/cast/basicFunction/auth/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.radio.cast.basicFunction.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.radio.cast.basicFunction.auth.dto.LoginRequest;
+import com.radio.cast.basicFunction.auth.dto.LoginResponse;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+  
+  private final AuthService authService;
+
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
+    String token = authService.login(loginRequest);
+    return ResponseEntity.ok(new LoginResponse(token));
+  }
+}
---
+++ backend/cast/cast/src/main/java/com/radio/cast/basicFunction/auth/dto/LoginResponse.java
@@ -0,0 +1,10 @@
+package com.radio.cast.basicFunction.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+  private String token;
+}
