b0d72ef7f9b59537585ccf411c9e8601304e09c4
kjsHome <was1239@gmail.com>
2026-01-05 00:28:08 +0900

    게시판 엔티티까지 작성 이후 게시판 로직 작성

---
+++ backend/cast/cast/src/main/java/com/radio/cast/post/entity/Post.java
@@ -0,0 +1,43 @@
+package com.radio.cast.post.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="Post")
+@Builder
+public class Post {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long postId;
+
+  @Column(nullable = false)
+  private String postTitle;
+
+  @Column(nullable = false)
+  private String postContent;
+
+  @Column(nullable = false)
+  private String postAuthor;
+
+  @Column(nullable = false)
+  private Date postDate;
+
+  @Column(nullable = true)
+  private Long postView;
+}
