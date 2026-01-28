73c47d7f69fa5961c124a544e6790946f6f8c1b1
kjsHome <was1239@gmail.com>
2026-01-29 03:56:45 +0900

    각 채널 인포 API 테스트 성공

--- frontend/src/components/radio/RadioInfo.jsx
+++ frontend/src/components/radio/RadioInfo.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "../../styles/RadioInfo.css";
+import { useParams } from "react-router-dom"; 
+import { useEffect, useState} from "react";
+import AxiosToken from "../../api/AxiosToken";
 
 const RadioInfo = ({ channel }) => {
+  // const [info, setInfo] = useState(null);
+  // const channelInfo = async () => {
+  //   const getData = await AxiosToken.get("/radio/detail",{
+  //     params : {radioChannelId : channel.radioChannelId}
+  //   });
+  //   setInfo(getData.data);
+  //   // console.log(getData.data)
+  // }
+  // useEffect(() =>{channelInfo()}, [channel.radioChannelId]);
+  // if (!info) {
+  //   return <div className="loading">데이터를 불러오는 중입니다...</div>;
+  // }
+  
   console.log(channel);
   if (!channel) {
     return <div className="radio-info">채널을 선택해주세요</div>;
